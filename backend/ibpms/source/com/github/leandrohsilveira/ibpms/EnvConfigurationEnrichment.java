fc5e2e10660280f9ae5fe8b5a8c6be18d3f48bac
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-04 17:21:30 -0200

    Enrich application configurations

---
+++ backend/ibpms/source/com/github/leandrohsilveira/ibpms/EnvConfigurationEnrichment.java
@@ -0,0 +1,35 @@
+package com.github.leandrohsilveira.ibpms;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.inject.Singleton;
+
+import kikaha.config.Config;
+import kikaha.config.ConfigEnrichment;
+import kikaha.config.MergeableConfig;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Singleton
+public class EnvConfigurationEnrichment implements ConfigEnrichment {
+
+	@Override
+	public Config enrich(Config mergeableConfig) {
+		MergeableConfig config = new MergeableConfig(mergeableConfig.toMap(), "");
+		String configFilePath = Optional.ofNullable(System.getenv("ENV_CONFIG_FILE")).orElse("/home/kikaha/ibpms/env.yml");
+		File configFile = new File(configFilePath);
+		if(configFile.exists()) {
+			try {
+				config.load(configFile);
+			} catch (IOException e) {
+				log.error("Unable to read or parse the environments configuration properties of file {}", configFilePath, e);
+			}
+		} else {
+			log.warn("The environments configuration properties file {} does not exists, using application default configurations", configFilePath);
+		}
+		return config; 
+	}
+
+}
