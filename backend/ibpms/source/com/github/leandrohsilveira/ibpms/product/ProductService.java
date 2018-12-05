d6d2a75c87f1939ab99d738b3eb4806e8460a28d
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-05 10:01:19 -0200

    Use managed DAO instance, add delete product service

--- backend/ibpms/source/com/github/leandrohsilveira/ibpms/product/ProductService.java
+++ backend/ibpms/source/com/github/leandrohsilveira/ibpms/product/ProductService.java
@@ -14,30 +14,33 @@ public class ProductService extends ModelService<Product> {
 	private static final long serialVersionUID = -9176173448022189383L;
 	
 	public UUID create(Product product) {
-		return withCommitableConnection(connection -> {
-			return new ProductDAO(connection).create(product);
+		return withCommitableDAO(ProductDAO::new, productDAO -> {
+			return productDAO.create(product);
 		});
 	}
 	
 	public Optional<Product> update(UUID uuid, Product product) {
-		return withCommitableConnection(connection -> {
-			return new ProductDAO(connection).update(uuid, product);
+		return withCommitableDAO(ProductDAO::new, productDAO -> {
+			return productDAO.update(uuid, product);
+		});
+	}
+	
+	public boolean delete(UUID uuid) {
+		return withCommitableDAO(ProductDAO::new, productDAO -> {
+			return productDAO.delete(uuid);
 		});
 	}
 	
 	public Optional<Product> findOne(UUID uuid) {
-		return withConnection(connection -> {
-			return new ProductDAO(connection).findOne(uuid);
+		return withDAO(ProductDAO::new, productDAO -> {
+			return productDAO.findOne(uuid);
 		});
 	}
 
 	public SearchResult<Product> search(ProductSearch search) {
-		return withConnection(connection -> {
-			return new ProductDAO(connection).search(search);
+		return withDAO(ProductDAO::new, productDAO -> {
+			return productDAO.search(search);
 		});
 	}
 
-	
-	
-	
 }
