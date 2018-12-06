1316729a04592fde141886a4ad37dba27169cf78
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-06 11:32:26 -0200

    Add currency field mask dependency

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,2 +1,14 @@
 riot.mount('*');
 numeral.locale('pt-br');
+
+window.ibpmsDefaultSimpleMaskMoneyConfig = {
+    "allowNegative": false,
+    "negativeSignAfter": false,
+    "decimalSeparator": ",",
+    "fixed": true,
+    "fractionDigits": 2,
+    "prefix": "",
+    "suffix": "",
+    "thousandsSeparator": ".",
+    "cursor": "move"
+};
