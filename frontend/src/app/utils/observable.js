e61faa5ec0ed0a9ada1baedd0a593cac5f8c760b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-12-18 17:16:07 -0200

    Improve observable docs

--- frontend/src/app/utils/observable.js
+++ frontend/src/app/utils/observable.js
@@ -1,16 +1,36 @@
 import riot from 'riot';
 
+/**
+ * A RXJS Subject like observable for RiotJS.
+ * 
+ * @param {any} initialValue the observable's initial value 
+ */
 function Observable(initialValue) {
 
     riot.observable(this);
 
+    /**
+     * observable's current value, avoid reading it directly. Use "subscribe" or "observe" functions instead.
+     */
     this.value = initialValue;
 
+    /**
+     * Literally set this observable next value, notifying all observers of it.
+     * 
+     * @param {any} value the next value of this observable
+     */
     this.next = (value) => {
         this.value = value;
         this.trigger('next', this.value);
     };
 
+    /**
+     * Merge complex objects as the next value of this observable. Be sure of always set a object to this observable.
+     * @param {any} value an object to merge with current state or a function to manually merge it returning the next value. 
+     *          If the current value and the provided value are objects, both will be merged as the next value.
+     *          If the provided value is a function, it will be called with the current value as parameter, and the returned value will be set as next value.
+     *          If the current value or the provided value are not an object or a function, it will simply set as this observable next value 
+     */
     this.merge = (value) => {
         if(typeof value === "function") {
             this.next({...value(this.value)});
@@ -19,8 +39,13 @@ function Observable(initialValue) {
         } else {
             this.next(value);
         }
-    }
+    };
 
+    /**
+     * Simply subscribe to changes
+     * @param {any} instance a riot observable instance.
+     * @param {Function} fn the observable change listener
+     */
     this.subscribe = (instance, fn) => {
 
         instance.on('mount', () => {
@@ -33,11 +58,21 @@ function Observable(initialValue) {
         });
 
         function onNext(value) {
-            console.log('Next observable called', this, value);
             fn(value);
         }
 
-    }
+    };
+
+    /**
+     * Subscribe to changes an map the changed value to a riot observable state.
+     * @param {any} instance a riot observable instance.
+     * @param {Function} mapper the mapper funtion to map value changes to observable instance state.
+     */
+    this.observe = (instance, mapper) => {
+        this.subscribe(instance, value => {
+            this.update(mapper(value));
+        });
+    };
 
 }
 
